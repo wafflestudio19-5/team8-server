045d04b1bf7c469932c3ae958b209e2d3021aadb
KangHyeoni <KangHyeoni@.com>
2021-12-28 08:41:19 +0900

    Fix: Change Location model

--- wafflemarket/location/models.py
+++ wafflemarket/location/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Location(models.Model):
     code = models.CharField(max_length=20, unique=True)
     place_name = models.CharField(max_length=255, unique=True)
-    neighbor_relationships = models.ManyToManyField('self', through='LocationNeighborhood', null=True, symmetrical=False)
+    neighbor_relationships = models.ManyToManyField('self', through='LocationNeighborhood', symmetrical=False)
 
 class LocationNeighborhood(models.Model):
     location = models.ForeignKey(Location, related_name='neighborhoods', on_delete=models.CASCADE)
