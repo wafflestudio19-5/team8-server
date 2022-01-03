d0f5f7519e0fb0fa096ea1fc9c1a79b81eb76a27
YoonahPark <86581647+YoonahPark@users.noreply.github.com>
2022-01-03 21:55:46 +0900

    Fix: Add location field to Article model

--- wafflemarket/article/models.py
+++ wafflemarket/article/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from user.models import User
+from location.models import Location
 
 class Article(models.Model):
     seller = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
     buyer = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey(Location, related_name='articles', null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=255)
     product_image = models.ImageField(blank=True, upload_to="photo/%Y/%m/%d") #image-upload 브랜치에서 수정 예정
