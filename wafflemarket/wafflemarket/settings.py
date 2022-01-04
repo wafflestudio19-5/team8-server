74a8a1894bce7e04b696af24b53a901e5040df91
YoonahPark <86581647+YoonahPark@users.noreply.github.com>
2022-01-05 03:10:33 +0900

    Remove: Delete migration files

--- wafflemarket/wafflemarket/settings.py
+++ wafflemarket/wafflemarket/settings.py
@@ -115,7 +115,6 @@ WSGI_APPLICATION = 'wafflemarket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
