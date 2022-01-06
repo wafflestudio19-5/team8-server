69dcbf4f25c59af193ef93bae6e859402027767f
YoonahPark <86581647+YoonahPark@users.noreply.github.com>
2022-01-06 13:48:39 +0900

    Change user/tests.py to add usercategory test

--- wafflemarket/user/tests.py
+++ wafflemarket/user/tests.py
@@ -118,3 +118,87 @@ class PostUserTestCase(TransactionTestCase):
 
         user_count = User.objects.count()
         self.assertEqual(user_count, 2)
+        
+        
+class PutUserCategoryTestCase(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = UserFactory(
+            phone_number='01033334444',
+            email='wafflemarket@test.com',
+            username='steve',
+        )
+        cls.user_token = 'JWT ' + jwt_token_of(User.objects.get(phone_number='01033334444'))
+        cls.put_data = {
+            'category' : '디지털기기',
+            'enabled' : 'False'
+        }
+        
+    def test_put_category_wrong_information(self):
+        # no token
+        data = self.put_data.copy()
+        response = self.client.put('/api/v1/user/category/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '1'*17)
+        
+        # invalid category
+        data = self.put_data.copy()
+        data['category'] = 'invalid'
+        response = self.client.put('/api/v1/user/category/', data=data, content_type='application/json', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['non_field_errors'][0], "카테고리가 부적절해요.")
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '1'*17)
+        
+        # invalid enabled
+        data = self.put_data.copy()
+        data['enabled'] = 'invalid'
+        response = self.client.put('/api/v1/user/category/', data=data, content_type='application/json', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['enabled'][0], "Must be a valid boolean.")
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '1'*17)
+        
+    def test_put_category_success(self):
+        
+        #successfully change enabled 'true' to 'false'
+        data = self.put_data.copy()
+        response = self.client.put('/api/v1/user/category/', data=data, content_type='application/json', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn("디지털기기", response.json()['category'])
+        self.assertIn("가구/인테리어", response.json()['category'])
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '0'+'1'*16)
+        
+        response = self.client.put('/api/v1/user/category/', data={'category':'가구/인테리어', 'enabled':'FALSE'}, content_type='application/json', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn("디지털기기", response.json()['category'])
+        self.assertNotIn("가구/인테리어", response.json()['category'])
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '00'+'1'*15)
+        
+        #successfully change enabled 'true' to 'false'
+        data = self.put_data.copy()
+        data['enabled'] = 'True'
+        response = self.client.put('/api/v1/user/category/', data=data, content_type='application/json', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("디지털기기", response.json()['category'])
+        self.assertNotIn("가구/인테리어", response.json()['category'])
+        user = User.objects.get(phone_number='01033334444')
+        self.assertEqual(user.interest, '10'+'1'*15)
+        
+class GetUserCategoryTestCase(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = UserFactory(
+            phone_number='01011112222',
+            email='wafflemarket@test.com',
+            username='steve',
+            interest = '11001100110011001'
+        )
+        cls.user_token = 'JWT ' + jwt_token_of(User.objects.get(phone_number='01011112222'))
+    def test_get_category_success(self):
+        response = self.client.get('/api/v1/user/category/', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['category'], ['디지털기기', '가구/인테리어', '여성의류', '게임/취미', '삽니다', '생활가전', '여성잡화', '남성패션/잡화', '기타 중고물품'])
