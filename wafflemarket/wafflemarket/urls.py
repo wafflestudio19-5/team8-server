d7a521db867d1f3893c8ac84be824a2d150db0c8
KangHyeoni <rkdgus0410@naver.com>
2022-01-08 07:16:45 +0900

    Style: Change code format

--- wafflemarket/wafflemarket/urls.py
+++ wafflemarket/wafflemarket/urls.py
@@ -13,21 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path, re_path
+from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
 
-from rest_framework.permissions import AllowAny
-
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
 
 # Swagger 설정
 schema_view = get_schema_view(
     openapi.Info(
         title="WaffleMarket API",
-        default_version='v1',
+        default_version="v1",
         description="와플마켓 API 목록",
         terms_of_service="https://www.google.com/policies/terms/",
     ),
@@ -36,10 +36,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('user.urls')),
-    path('api/v1/', include('article.urls')),
-    path('api/v1/', include('location.urls'))
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("user.urls")),
+    path("api/v1/", include("article.urls")),
+    path("api/v1/", include("location.urls")),
 ]
 
 if settings.DEBUG:
@@ -48,7 +48,19 @@ if settings.DEBUG:
 # DEBUG = True 일때만 Swagger 사용
 if settings.DEBUG:
     urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^redoc/$",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
     ]
