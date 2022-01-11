1584181fcf06c2f0f9e23ac8867b8d1cc26889c5
KangHyeoni <rkdgus0410@naver.com>
2022-01-12 00:02:39 +0900

    Style: Change code format of article app to black

--- wafflemarket/article/models.py
+++ wafflemarket/article/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+
 from user.models import User
-from location.models import Location
 from user.services import upload_product_image
+from location.models import Location
+
 
 class Article(models.Model):
-    seller = models.ForeignKey(User, related_name='articles_sold', null=True, on_delete=models.SET_NULL)
-    buyer = models.ForeignKey(User, related_name='articles_bought', null=True, on_delete=models.SET_NULL)
-    location = models.ForeignKey(Location, related_name='articles', null=True, on_delete=models.SET_NULL)
+    seller = models.ForeignKey(
+        User, related_name="articles_sold", null=True, on_delete=models.SET_NULL
+    )
+    buyer = models.ForeignKey(
+        User, related_name="articles_bought", null=True, on_delete=models.SET_NULL
+    )
+    location = models.ForeignKey(
+        Location, related_name="articles", null=True, on_delete=models.SET_NULL
+    )
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=255)
     product_image = models.ImageField(blank=True, upload_to=upload_product_image)
@@ -14,22 +22,39 @@ class Article(models.Model):
     price = models.PositiveBigIntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     sold_at = models.DateTimeField(null=True, default=None)
-    
-    def update(self, title=None, content=None, product_image=None, category=None, price=None):
-        if title is not None: self.title = title
-        if content is not None: self.content = content
-        if product_image is not None: self.product_image = product_image
-        if category is not None: self.category = category
-        if price is not None: self.price = price
+
+    def update(
+        self, title=None, content=None, product_image=None, category=None, price=None
+    ):
+        if title is not None:
+            self.title = title
+        if content is not None:
+            self.content = content
+        if product_image is not None:
+            self.product_image = product_image
+        if category is not None:
+            self.category = category
+        if price is not None:
+            self.price = price
+
 
 class Comment(models.Model):
-    commenter = models.ForeignKey(User, related_name='comments', null=True, on_delete=models.SET_NULL)
-    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', related_name='replies', null=True, on_delete=models.SET_NULL)
+    commenter = models.ForeignKey(
+        User, related_name="comments", null=True, on_delete=models.SET_NULL
+    )
+    article = models.ForeignKey(
+        Article, related_name="comments", on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        "self", related_name="replies", null=True, on_delete=models.SET_NULL
+    )
     content = models.CharField(max_length=120, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, default=None)
 
+
 class ProductImage(models.Model):
-    article = models.ForeignKey(Article, related_name='product_images', null=False, on_delete=models.CASCADE)
+    article = models.ForeignKey(
+        Article, related_name="product_images", null=False, on_delete=models.CASCADE
+    )
     product_image = models.ImageField(upload_to=upload_product_image)
