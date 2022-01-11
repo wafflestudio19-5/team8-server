77d7170a25cb0526c3d281375150e1cdd6e79f1c
KangHyeoni <rkdgus0410@naver.com>
2022-01-11 23:46:49 +0900

    Style: Change code format of user apps to black

--- wafflemarket/user/services.py
+++ wafflemarket/user/services.py
@@ -1,15 +1,23 @@
 import os
 import random
 import string
+
 from django.utils.timezone import now
- 
+
+
 def upload_image(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
 
-    return '%s' % (now().strftime('%Y%m%d%H%M%S')+'_'+''.join(random.choices(string.ascii_lowercase + string.digits, k=10)))
+    return "%s" % (
+        now().strftime("%Y%m%d%H%M%S")
+        + "_"
+        + "".join(random.choices(string.ascii_lowercase + string.digits, k=10))
+    )
+
 
 def upload_profile_image(instance, filename):
-    return 'profile_image/'+upload_image(instance, filename)
+    return "profile_image/" + upload_image(instance, filename)
+
 
 def upload_product_image(instance, filename):
-    return 'product_image/'+upload_image(instance, filename)
+    return "product_image/" + upload_image(instance, filename)
