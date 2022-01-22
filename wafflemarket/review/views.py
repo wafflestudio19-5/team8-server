4f9cc6297c6f96e953ffbd9ed2abeb9d8bb737ac
YoonahPark <cufft@naver.com>
2022-01-22 18:03:26 +0900

    Feat: Add review GET api

--- wafflemarket/review/views.py
+++ wafflemarket/review/views.py
@@ -4,11 +4,10 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django.db import IntegrityError
-from django.shortcuts import render
+from django.db.models import Q
 
-from .models import Review, Temparature
-from .serializers import ReviewArticleSerializer, ReviewArticleValidator, ReviewUserSerializer, ReviewUserValidator
+from .models import Review
+from .serializers import ReviewArticleSerializer, ReviewArticleValidator, ReviewUserSerializer, ReviewUserValidator, ReviewSerializer, UserReviewSerializer
 from user.models import User
 from article.models import Article
 
@@ -244,18 +243,70 @@ class ReviewViewSet(viewsets.GenericViewSet):
     serializer_class = ReviewUserSerializer
     queryset = Review.objects.all()
     
-    def retrieve(self, request, pk):
+    code_good_manner = {
+            0 : "친절하고 매너가 좋아요.",
+            1 : "시간 약속을 잘 지켜요.",
+            2 : "응답이 빨라요.",
+            
+            3 : "상품상태가 설명한 것과 같아요.",
+            4 : "나눔을 해주셨어요.",
+            5 : "상품설명이 자세해요.",
+            6 : "좋은 상품을 저렴하게 판매해요.",
+            
+            7 :"제가 있는 곳까지 와서 거래했어요.",
+    }
+    
+    good_manner_cnt = {
+            "친절하고 매너가 좋아요." : set(),
+            "시간 약속을 잘 지켜요." : set(),
+            "응답이 빨라요." : set(),
+            
+            "상품상태가 설명한 것과 같아요." : set(),
+            "나눔을 해주셨어요." : set(),
+            "상품설명이 자세해요." : set(),
+            "좋은 상품을 저렴하게 판매해요." : set(),
+            
+            "제가 있는 곳까지 와서 거래했어요." : set(),
+        } 
+
+    @action(detail=True, methods=["GET",])
+    def review(self, request, pk):
         if User.objects.filter(pk=pk).exists():
             reviewyee = User.objects.get(pk=pk)
+        else:
+            return Response(
+                {"존재하지 않는 사용자입니다."}, status=status.HTTP_404_NOT_FOUND
+                )
+        reviews = Review.objects.filter(Q(review_type="seller") | Q(review_type="buyer"), reviewyee=reviewyee)
+        return Response(ReviewSerializer(reviews, many=True).data, status=status.HTTP_200_OK)
+    
+    
+    @action(detail=True, methods=["GET",])
+    def manner(self, request, pk):
+        if User.objects.filter(pk=pk).exists():
+            reviewyee = User.objects.get(pk=pk)
+        else:
+            return Response(
+                {"존재하지 않는 사용자입니다."}, status=status.HTTP_404_NOT_FOUND
+                )
+        
+        reviews = Review.objects.filter(reviewyee=reviewyee, manner_type="good").all()
+        for review in reviews:
+            manner = review.manner
+            reviewer = review.reviewer
+            for i, v in enumerate(manner):
+                if v=="1":
+                    self.good_manner_cnt[self.code_good_manner[i]].add(reviewer)
+        for manner in self.good_manner_cnt:
+            self.good_manner_cnt[manner] = len(self.good_manner_cnt[manner])
+        return Response(self.good_manner_cnt, status=status.HTTP_200_OK)
+    
+    
+    def retrieve(self, request, pk):
+        if User.objects.filter(pk=pk).exists():
+            user = User.objects.get(pk=pk)
         else:
             Response(
                 {"존재하지 않는 사용자입니다."}, status=status.HTTP_404_NOT_FOUND
                 )
-   
-'''   
-    @action(detail=True, methods=["GET",]) #response: 보낸 후기 serializing
-    def review(self, request):
-        
-    @action(detail=True, methods=["GET",]) #response: 보낸 후기 serializing
-    def review(self, request):
-    '''
+        return Response(UserReviewSerializer(user).data, status=status.HTTP_200_OK)
