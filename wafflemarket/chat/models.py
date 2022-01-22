beb20fd6a5ad16c8a2d61ebb2a8733173f141a50
KangHyeoni <rkdgus0410@naver.com>
2022-01-22 12:31:21 +0900

    Fix: Change chatroom name unique

--- wafflemarket/chat/models.py
+++ wafflemarket/chat/models.py
@@ -6,7 +6,7 @@ from article.models import Article
 
 
 class ChatRoom(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     article = models.ForeignKey(Article, related_name="chatrooms", null=True, on_delete=models.SET_NULL)
     seller = models.ForeignKey(User, related_name="chatrooms_sold", null=True, on_delete=models.SET_NULL)
     buyer = models.ForeignKey(User, related_name="chatrooms_bought", null=True, on_delete=models.SET_NULL)
